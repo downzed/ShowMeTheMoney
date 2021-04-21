3bf277bfef374c3dfa5c20c771989863875cae66
Ziv Zerr <downzed@gmail.com>
2021-04-21 11:31:33 +0300

    design

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,24 +1,25 @@
 chrome.storage.local.get([consts.LOCALSTORAGE_ITEM_NAME], (result) => {
 	let list = result[consts.LOCALSTORAGE_ITEM_NAME] || [];
-	const wrapper = document.getElementById('--reOpen-ext-popup-wrapper');
+	const wrapper = document.getElementById('--reOpen-ext-res-list');
 	if (list.length > 0) {
 		const listContainer = document.createElement('ul');
 		listContainer.setAttribute('id', '--reOpen-ext-list');
 		wrapper.appendChild(listContainer);
 		list.forEach(item => {
-			const li = document.createElement('li');
-			const innerHTML = `
-				<div class="ext-list-item">
-					<div class="ext-list-item-details>
-						<img class="ext-rest-logo" src='https://d25t2285lxl5rf.cloudfront.net/images/shops/default.png'></img>
-						${item.resName}
+			if (item) {
+				const li = document.createElement('li');
+				const innerHTML = `
+					<div class="ext-list-item">
+						<div class="ext-list-item-details">
+							<img class="ext-rest-logo" src='${item.resImg}'/>
+							${item.resName}
+						</div>
+						<img class="ext-close-icon" src='../icons/remove-icon.png'></img>
 					</div>
-					<img class="ext-close-icon" src='../icons/remove-icon.png'></img>
-				</div>
-			`
-			li.innerHTML = innerHTML;
-			listContainer.appendChild(li);
-			console.log(li)
+				`
+				li.innerHTML = innerHTML;
+				listContainer.appendChild(li);
+			}
 		});
 		chrome.extension.sendMessage({ flash: false });
 	}
