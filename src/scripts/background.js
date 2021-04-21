eebfa40133d26b20c77444099c9b3500aa56b7dd
Ziv Zerr <downzed@gmail.com>
2021-04-21 14:27:39 +0300

    popup states

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -6,6 +6,9 @@ chrome.storage.local.clear(function () {
 });
 
 chrome.extension.onMessage.addListener(function (message, sender, reply) {
+  if (message.refreshLocalStorage) {
+    chrome.storage.local.set({ [consts.LOCALSTORAGE_ITEM_NAME]: message.list });
+  }  
   if (message.flash) {
     chrome.browserAction.setBadgeText({text: '+'})
     return;
@@ -24,17 +27,16 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   }
 
   // tab.url = https://www.10bis.co.il/next/restaurants/menu/delivery/26199/%D7%A7%D7%A8%D7%9F-%D7%91%D7%93%D7%99%D7%A7%D7%95%D7%AA
-  const parts = tab.url.split('/');
+  const parts = tab.url.split('/'); 
   const restaurantName = parts[parts.length - 1];
   const restaurantId = parts[parts.length - 2];
   const deliveryMethod = parts[parts.length - 3];
-
 
   chrome.tabs.sendMessage(tabId, {
     message: consts.CUSTOM_EVENTS.IN_RESTAURANT_PAGE,
     deliveryMethod,
     restaurantId,
-    restaurantName,
+    restaurantName
   });
 
 });
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,24 +1,26 @@
-chrome.storage.local.get([consts.LOCALSTORAGE_ITEM_NAME], (result) => {
-	let list = result[consts.LOCALSTORAGE_ITEM_NAME] || [];
-	const subtext = document.getElementById('ext-popup-subtext');
-	const wrapper = document.getElementById('--reOpen-ext-res-list');
-	if (list.length === 0) {
-		const trackButton = document.createElement('div');
-		trackButton.setAttribute('class', 'ext-list-track-btn');
-		trackButton.innerHTML = 'Track this restaurant';
-		subtext.innerHTML = 'No restaurants being tracked. Start adding!';
-		wrapper.appendChild(trackButton);
-		return;
-	} 
-	subtext.innerHTML = 'Currently keeping an eye on:';
+function createEmptyPage(wrapperElement, subtext) {
+	const trackButton = document.createElement('div');
+	trackButton.setAttribute('class', 'ext-list-track-btn');
+	trackButton.innerHTML = 'Track this restaurant';
+	chrome.storage.local.get([consts.LOCALSTORAGE_QUE_NAME], (result) => {
+		const resItem = result[consts.LOCALSTORAGE_QUE_NAME] || {};
+		if (resItem) {
+			trackButton.onclick = function () {
+				let list = [];
+				list.push(resItem);
+				chrome.storage.local.set({ [consts.LOCALSTORAGE_ITEM_NAME]: list });
+				setTimeout(function () {
+					window.location.reload()
+				}, 100)
+			}
+		}
+	})
+	subtext.innerHTML = 'No restaurants being tracked. Start adding!';
+	wrapperElement.appendChild(trackButton);
+}
 
-	const listContainer = document.createElement('ul');
-	listContainer.setAttribute('id', '--reOpen-ext-list');
-	
-	wrapper.appendChild(listContainer);
-	
+function createListItems(list, containerElement) {
 	list.forEach(item => {
-		console.log(item)
 		if (item) {
 			const li = document.createElement('li');
 			const innerHTML = `
@@ -32,9 +34,46 @@ chrome.storage.local.get([consts.LOCALSTORAGE_ITEM_NAME], (result) => {
 			`;
 
 			li.innerHTML = innerHTML;
-			listContainer.appendChild(li);
+			containerElement.appendChild(li);
+			setTimeout(function () {
+				const closeIcon = document.getElementsByClassName('ext-close-icon')[0];
+				closeIcon.onclick = e => handleRemoveItem(item, list);
+			}, 100)
 		}
 	});
+}
+
+function handleRemoveItem(removeItem, list) {
+	if (list.indexOf(removeItem) > -1) {
+		list = list.filter(item => item.resId !== removeItem.resId);
+		chrome.extension.sendMessage({ refreshLocalStorage: true, list });
+		setTimeout(function(){
+			window.location.reload()
+		}, 100)
+	}
+	return list;
+}
+
+chrome.storage.local.get([consts.LOCALSTORAGE_ITEM_NAME], (result) => {
+	
+	let list = result[consts.LOCALSTORAGE_ITEM_NAME] || [];
+
+	const subtext = document.getElementById('ext-popup-subtext');
+	const wrapper = document.getElementById('--reOpen-ext-res-list');
+
+	if (list.length === 0) {
+		createEmptyPage(wrapper, subtext)
+		return;
+	} 
+
+	const listContainer = document.createElement('ul');
+	listContainer.setAttribute('id', '--reOpen-ext-list');
+	
+	subtext.innerHTML = 'Currently keeping an eye on:';
+
+	wrapper.appendChild(listContainer);
+
+	createListItems(list, listContainer)
 
 	chrome.extension.sendMessage({ flash: false });
 	
