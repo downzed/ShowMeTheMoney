99ecdfff936f93444bc3d3a9a41435bda918d1b8
Ziv Zerr <downzed@gmail.com>
2021-04-21 11:11:47 +0300

    popup

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,12 +1,25 @@
 chrome.storage.local.get([consts.LOCALSTORAGE_ITEM_NAME], (result) => {
 	let list = result[consts.LOCALSTORAGE_ITEM_NAME] || [];
-	let container = document.getElementById('--reOpen-ext-list')
+	const wrapper = document.getElementById('--reOpen-ext-popup-wrapper');
 	if (list.length > 0) {
+		const listContainer = document.createElement('ul');
+		listContainer.setAttribute('id', '--reOpen-ext-list');
+		wrapper.appendChild(listContainer);
 		list.forEach(item => {
-			const li = document.createElement('li')
-			li.innerHTML = item.resName;
-			container.appendChild(li)
+			const li = document.createElement('li');
+			const innerHTML = `
+				<div class="ext-list-item">
+					<div class="ext-list-item-details>
+						<img class="ext-rest-logo" src='https://d25t2285lxl5rf.cloudfront.net/images/shops/default.png'></img>
+						${item.resName}
+					</div>
+					<img class="ext-close-icon" src='../icons/remove-icon.png'></img>
+				</div>
+			`
+			li.innerHTML = innerHTML;
+			listContainer.appendChild(li);
+			console.log(li)
 		});
-		chrome.extension.sendMessage({ flash: false })
+		chrome.extension.sendMessage({ flash: false });
 	}
 });
